6ecabe7fdb44eff7ce6b0f1e6560e7779a954b66
Joshua Portero <77929472+joshuaportero@users.noreply.github.com>
2023-10-13 12:26:11 -0400

    ✅ Add support for automatic versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.io.ByteArrayOutputStream
 
 plugins {
-    id("java")
+    id("java-library")
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "xyz.feuxy.neon"
-version = "2.1.4-SNAPSHOT"
+version = "3.0.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
 }
 
 tasks.withType<JavaCompile> {
@@ -18,27 +18,51 @@ tasks.withType<JavaCompile> {
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven("https://oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
 
+    compileOnly("org.jetbrains:annotations:24.0.0")
+
     compileOnly("org.projectlombok:lombok:1.18.28")
     annotationProcessor("org.projectlombok:lombok:1.18.28")
 }
 
+val determinePatchVersion: () -> Int = {
+    val tagInfo = ByteArrayOutputStream()
+    try {
+        exec {
+            commandLine("git", "describe", "--tags")
+            standardOutput = tagInfo
+        }
+        if (!tagInfo.toString().contains("-")) {
+            0
+        } else {
+            tagInfo.toString().split("-")[1].toInt()
+        }
+    } catch (e: Exception) {
+        0
+    }
+}
+
+val majorVersion = "3"
+val minorVersion = "0"
+val patchVersion = determinePatchVersion()
+val apiVersion = "$majorVersion.$minorVersion"
+val fullVersion = "$apiVersion.$patchVersion"
+
 tasks.register<Copy>("updatePluginYml") {
     from(sourceSets.main.get().resources.srcDirs) {
         include("**/plugin.yml")
-        expand("version" to project.version)
+        expand("version" to fullVersion)
     }
     into(File(buildDir, "resources/main"))
 }
 
 tasks.named<ShadowJar>("shadowJar") {
     dependsOn("updatePluginYml")
-    archiveFileName.set("${project.name}-${project.version}.jar")
+    archiveFileName.set("${project.name}-${fullVersion}.jar")
 }
